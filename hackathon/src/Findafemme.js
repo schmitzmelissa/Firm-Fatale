2fbe305087ef42773018097f89d365d2a9f9d524
Heejae Kim <h255kim@edu.uwaterloo.ca>
2019-03-03 00:39:03 -0500

    Update FindAFemme.js

--- hackathon/src/Findafemme.js
+++ hackathon/src/Findafemme.js
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
 import './App.css';
 
-const mapStyles = {
-  width: '80%',
-  height: '80%'
-};
 
-export class FineAFemme extends Component {
+class FineAFemme extends Component {
     render() {
         return (
-          <div>
-          <body>
             <div className="App">
-                <header className = "App-header">
-                <p>Find-A-Femme</p>
-                <Map
-                    google={this.props.google}
-                    zoom={14}
-                    style={mapStyles}
-                    initialCenter={{
-                     lat: -1.2884,
-                     lng: 36.8233
-                    }}
-                  />
-                </header>
+                FineAFemme
             </div>
-            </body>
-            </div>
-
-
-
-
         );
     }
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDHPclKWBWCitdPzcV29V-OpQ4YvQQ6k5A'
-})(FineAFemme);
+export default FineAFemme;
